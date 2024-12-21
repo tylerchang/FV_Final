d76bac5d314b45dc1fd8310a1933990f0e5fa79d
Tim Chang <tyc2118@columbia.edu>
2024-12-21 16:18:05 -0500

    use basic strategy for equivalence checking as per jasper slides

--- alu_equivalence.tcl
+++ alu_equivalence.tcl
@@ -6,6 +6,7 @@ check_sec -elaborate -spec -top alu
 check_sec -elaborate -imp -top structural_alu
 check_sec -setup
 check_sec -auto_map_reset_x_values on
+set_sec_autoprove_strategy basic
 
 clock clk
 reset ~rst_n
