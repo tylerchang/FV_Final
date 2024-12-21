d5ade6a5145ffd03129caf4b8e59590377d3ce8a
Tim Chang <tyc2118@columbia.edu>
2024-12-20 21:21:51 -0500

    add structural alu

--- jgproject/.tmp/.initCmds.tcl
+++
@@ -1,3 +0,0 @@
-@puts {INFO: successfully checked out licenses "jasper_interactive" and "jasper_fao".}
-@puts {INFO: reading configuration file "/user/stud/fall24/tyc2118/.config/cadence/jasper.conf".}
-@get_message -clear
